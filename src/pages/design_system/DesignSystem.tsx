962c2336ed2c74141c37b59cf31606b390d499c5
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-04-28 02:41:37 +0300

    (setup): Add typography system to global styles

--- src/pages/design_system/DesignSystem.tsx
+++ src/pages/design_system/DesignSystem.tsx
@@ -1,3 +1,46 @@
 export const DesignSystem = () => {
-  return <div>DesignSystem</div>;
+  return (
+    <div>
+      <h2>DesignSystem</h2>
+      <h1>Morbi interdum mollis sapien</h1>
+      <h2>Donec nec justo eget felis facilisis</h2>
+      <h3>Nunc sem lacus accum</h3>
+      <h4>interdum consectetuer</h4>
+      <h5>nascetur ridiculus mus</h5>
+      <h6>natoque penatibus et</h6>
+      <span className="overline">LOREM IPSUM</span>
+      <p className="sub_title">Fusce ut est sed dolor gravida convallis</p>
+      <article>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus
+        hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet
+        vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin
+        laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu
+        nibh. Nullam mollis. Ut justo. Suspendisse potenti. Sed egestas, ante et
+        vulputate volutpat, eros pede semper est, vitae luctus metus libero eu
+        augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida
+        id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper
+        lorem at felis. Vestibulum volutpat, lacus a ultrices sagittis, mi neque
+        euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus pede arcu,
+        dapibus eu, fermentum et, dapibus sed, urna. Morbi interdum mollis
+        sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet,
+        lectus arcu pulvinar risus, vitae facilisis libero dolor a purus. Sed
+        vel lacus. Mauris nibh felis, adipiscing varius, adipiscing in, lacinia
+        vel, tellus. Suspendisse ac urna. Etiam pellentesque mauris ut lectus.
+        Nunc tellus ante, mattis eget, gravida vitae, ultricies ac, leo. Integer
+        leo pede, ornare a, lacinia eu, vulputate vel, nisl. Suspendisse mauris.
+        Fusce accumsan mollis eros. Pellentesque a diam sit amet mi ullamcorper
+        vehicula. Integer adipiscing risus a sem. Nullam quis massa sit amet
+        nibh viverra malesuada. Nunc sem lacus, accumsan quis, faucibus non,
+        congue vel, arcu. Ut scelerisque hendrerit tellus. Integer sagittis.
+        Vivamus a mauris eget arcu gravida tristique. Nunc iaculis mi in ante.
+        Vivamus imperdiet nibh feugiat est. Ut convallis, sem sit amet interdum
+        consectetuer, odio augue aliquam leo, nec dapibus tortor nibh sed augue.
+        Integer eu magna sit amet metus fermentum posuere. Morbi sit amet nulla
+        sed dolor elementum imperdiet. Quisque fermentum. Cum sociis natoque
+        penatibus et magnis xdis parturient montes, nascetur ridiculus mus.
+        Pellentesque adipiscing eros ut libero. Ut condimentum mi vel tellus.
+        Suspendisse laoreet. Fusce ut est sed dolor gravida convallis.
+      </article>
+    </div>
+  );
 };
