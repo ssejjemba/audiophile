164e8a3419969fea3f81a89171d49eb40d9e8b4d
DESKTOP-G60BJ3E\Ssonko Jimmy <ssembatyadennis25@gmail.com>
2023-05-08 12:34:33 +0300

    style(Categories): add the categories container styles

--- src/sections/categories/Categories.tsx
+++ src/sections/categories/Categories.tsx
@@ -1,13 +1,30 @@
 import { styled } from "@stitches/react";
 
-const CategoriesContainer = styled("section", {
+const CategoriesSection = styled("section", {
   width: "100%",
   height: "100vh",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
   paddingLeft: "165px",
   paddingRight: "165px",
   background: "$gray400",
 });
 
+const CategoriesContainer = styled("section", {
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "flex-start",
+  justifyContent: "space-evenly",
+  width: "100%",
+  height: "284px",
+  background: "OrangeRed",
+});
+
 export const Categories = () => {
-  return <CategoriesContainer></CategoriesContainer>;
+  return (
+    <CategoriesSection>
+      <CategoriesContainer></CategoriesContainer>
+    </CategoriesSection>
+  );
 };
