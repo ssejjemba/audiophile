478bd916c20f5d4e54c5aec6879e01c5e0b88370
DESKTOP-G60BJ3E\Ssonko Jimmy <ssembatyadennis25@gmail.com>
2023-05-13 13:48:56 +0300

    feat(Category-slot): add the earphone product page

--- src/pages/categories/CategoryHeadphones.tsx
+++ src/pages/categories/CategoryHeadphones.tsx
@@ -1,9 +1,15 @@
 import { CategoryHero } from "../../sections/category-hero/CategoryHero";
+import productData from "../../data/product.json";
 
 export const CategoryHeadphones = () => {
   return (
     <>
-      <CategoryHero />
+      <CategoryHero
+        title={productData[0].productName.slice(
+          productData[0].productName.indexOf(" "),
+          -1
+        )}
+      />
     </>
   );
 };
--- src/sections/category-hero/CategoryHero.tsx
+++ src/sections/category-hero/CategoryHero.tsx
@@ -1,16 +1,36 @@
 import { styled } from "@stitches/react";
 import { NavigationBar } from "../navigation/NavigationBar";
 
+export type CategoryHeroProps = {
+  title: string;
+};
+
 const CategoryHeroSection = styled("section", {
   width: "100%",
-  height: "50vh",
+  height: "40vh",
+  padding: "0 100px",
   background: "$secondary",
 });
 
-export const CategoryHero = () => {
+const HeroHeadingContainer = styled("div", {
+  width: "100%",
+  height: "auto",
+  marginTop: "100px",
+  display: "flex",
+  justifyContent: "center",
+});
+
+const HeroHeading = styled("h2", {
+  color: "$white",
+});
+
+export const CategoryHero = ({ title }: CategoryHeroProps) => {
   return (
     <CategoryHeroSection>
       <NavigationBar />
+      <HeroHeadingContainer>
+        <HeroHeading>{title}</HeroHeading>
+      </HeroHeadingContainer>
     </CategoryHeroSection>
   );
 };
