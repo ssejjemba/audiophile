331d251e5e70526f188417f7d8a5f203604968ba
DESKTOP-G60BJ3E\Ssonko Jimmy <ssembatyadennis25@gmail.com>
2023-05-13 12:36:39 +0300

    style(Error): Improve styles on the Error Page.

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -1,4 +1,23 @@
 import { useRouteError } from "react-router-dom";
+import { styled } from "@stitches/react";
+
+const ErrorPageContainer = styled("div", {
+  width: "100%",
+  height: "100vh",
+  padding: "0 100px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+const ErrorText = styled("p", {
+  color: "$primary",
+  fontSize: "18px",
+  fontWeight: "500",
+  lineHeight: "25px",
+  marginTop: "10px",
+});
 
 export default function ErrorPage() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -6,12 +25,9 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <ErrorPageContainer id="error-page">
+      <h3>Oops! Sorry, an unexpected error has occurred.</h3>
+      <ErrorText>{error.statusText || error.message}</ErrorText>
+    </ErrorPageContainer>
   );
 }
